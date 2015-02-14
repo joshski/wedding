2f756f8cbde0a8c1872c957313d4b03a1f354b30
Josh Chisholm <joshuachisholm@gmail.com>
2015-02-14 12:41:08 +0000

    more style

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,11 +1,8 @@
 $(window).scroll(function(){
   var width = $(window).width();
-
   if (width > 1024) {
     var body = $('body');
     var top = $(this).scrollTop();
-    $('.first .big-logo-row').css('top', top / 2);
-
-
+    $('#first .big-logo-row').css('top', top / 2);
   }
 });
