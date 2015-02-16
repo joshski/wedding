5af1353fff247041bda2adafdc0bdb6ae0f88e7e
leomdivani <leomdiv@gmail.com>
2015-02-16 10:27:08 +0000

    proposal path

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Wedding::Application.routes.draw do
   resources "paypal_payments", only: [:create]
   root 'home#home'
 
+
+  match '/proposal',    to: 'home#proposal',   via: 'get'
+  match '/contacts', to: 'contacts#new', via: 'get'
+
 end
